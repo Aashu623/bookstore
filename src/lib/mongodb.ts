95d27cc0ff30c5db0d918c1188847d1135590367
Aashish Kushwah <aashukushwah53678@gmail.com>
2024-12-20 00:34:00 +0530

    Fix database connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,8 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectToDatabase = async () => {
-  if (mongoose.connection.readyState === 1) return mongoose.connection.asPromise();
-  return mongoose.connect(process.env.MONGODB_URI!);
-};
+const MONGODB_URI = process.env.MONGODB_URI || "your-mongodb-connection-string";
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable.");
+}
+
+async function connectToDatabase() {
+  try {
+    const connection = await mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+    console.log("Successfully connected to MongoDB.");
+    return connection;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    throw error;
+  }
+}
 
 export default connectToDatabase;
